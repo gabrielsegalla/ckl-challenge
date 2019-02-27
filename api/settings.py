4cc77f864bc334b248b51a7cb597aa898acfaef4
Gabriel Rodrigues Segalla <gabriel.segalla@agrosatelite.com.br>
2019-02-27 14:42:43 -0300

    change database

--- api/settings.py
+++ api/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = "api.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#     }
+# }
 
 
 # Password validation
@@ -153,3 +153,6 @@ STATIC_URL = "/static/"
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+
+
+DATABASES = {"default": {"ENGINE": "django.db.backends.postgresql_psycopg2"}}
