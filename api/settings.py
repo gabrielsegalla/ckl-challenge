d5ece7a7d56aeebe611584a34f2988704afa97a7
Gabriel Rodrigues Segalla <gabriel.segalla@agrosatelite.com.br>
2019-02-27 11:17:34 -0300

    Adjust Heroku

--- api/settings.py
+++ api/settings.py
@@ -138,10 +138,15 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = ("http://localhost:3000",)
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_ROOT, "staticfiles")
 STATIC_URL = "/static/"
-STATIC_DIRS = "static"
-STATICFILES_DIR = [STATIC_DIRS]
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, "static"),)
+
+STATICFILES_STORAGE = "whitenoise.django.GzipManifestStaticFilesStorage"
+
