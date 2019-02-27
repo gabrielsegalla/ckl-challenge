1d63acb43eff2e9fb910927ea5318265fc477d2f
Gabriel Rodrigues Segalla <gabriel.segalla@agrosatelite.com.br>
2019-02-27 15:06:39 -0300

    separated frontend

--- api/authentication/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-#from authentication.models import User
-#from authentication.serializers import UserSerializer
-
-
-#class UsersViewSet(viewsets.ModelViewSet):
-#    queryset = User.objects.all()
-#    serializer_class = UserSerializer
